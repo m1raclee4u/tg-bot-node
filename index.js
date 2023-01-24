725af3e6125628b003ae866cae9ba43c0ba868bc
m1raclee4u <64864409+m1raclee4u@users.noreply.github.com>
2023-01-24 15:30:19 +0300

    newadress

--- index.js
+++ index.js
@@ -15,14 +15,14 @@ bot.on('message', async (msg) => {
     const chatId = msg.chat.id;
     const text = msg.text;
 
-    // if(text === '/start') {
-    //     await bot.sendMessage(chatId, 'Ниже появится кнопка, заполни форму', {
-    //         reply_markup: {
-    //             keyboard: [
-    //                 [{text: 'Заполнить форму', web_app: {url: webAppUrl + '/form'}}]
-    //             ]
-    //         }
-        })
+    if(text === '/start') {
+        // await bot.sendMessage(chatId, 'Ниже появится кнопка, заполни форму', {
+        //     reply_markup: {
+        //         keyboard: [
+        //             [{text: 'Заполнить форму', web_app: {url: webAppUrl + '/form'}}]
+        //         ]
+        //     }
+        // })
 
         await bot.sendMessage(chatId, 'Заходи в наш интернет магазин по кнопке ниже', {
             reply_markup: {
