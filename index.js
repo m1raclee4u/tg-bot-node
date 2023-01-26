ae560b84e76611efceae2fa3e986554a9bf4dbfb
m1raclee4u <64864409+m1raclee4u@users.noreply.github.com>
2023-01-26 04:44:47 +0300

    ip

--- index.js
+++ index.js
@@ -53,20 +53,16 @@ bot.on('message', async (msg) => {
 
 app.post('/web-data', async (req, res) => {
     const {queryId, products = [], totalPrice} = req.body;
-    try {
-        await bot.answerWebAppQuery(queryId, {
+    bot.answerWebAppQuery(queryId, {
             type: 'article',
             id: queryId,
             title: 'Успешная покупка',
             input_message_content: {
                 message_text: ` Поздравляю с покупкой, вы приобрели товар на сумму ${totalPrice}, ${products.map(item => item.title).join(', ')}`
             }
+            
         })
-        return res.status(200).json({});
-    } catch (e) {
-        await bot.sendMessage(chatId, 'Спасибо за обратную связь!')
-        return res.status(500).json({})
-    }
+    res.status(200).json({});    
 })
 
 const PORT = 8080;
