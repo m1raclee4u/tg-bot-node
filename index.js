ac8c3ff28d2d5ef63f192e0164ad73fff6e78990
m1raclee4u <64864409+m1raclee4u@users.noreply.github.com>
2023-01-28 19:38:57 +0300

    ssl

--- index.js
+++ index.js
@@ -29,61 +29,7 @@ const PORT = 8080;
 
 https.createServer(options, (req, res) => {
     
-    bot.on('message', async (msg) => {
-        const chatId = msg.chat.id;
-        const text = msg.text;
-    
-        if(text === '/start') {
-            await bot.sendMessage(chatId, 'Ниже появится кнопка, заполни форму', {
-                reply_markup: {
-                    keyboard: [
-                        [{text: 'Заполнить форму', web_app: {url: webAppUrl + '/form'}}]
-                    ]
-                }
-            })
-    
-            await bot.sendMessage(chatId, 'Заходи в наш интернет магазин по кнопке ниже', {
-                reply_markup: {
-                    inline_keyboard: [
-                        [{text: 'Сделать заказ', web_app: {url: webAppUrl}}]
-                    ]
-                }
-            })
-        }
-    
-        if(msg?.web_app_data?.data) {
-            try {
-                const data = JSON.parse(msg?.web_app_data?.data)
-                console.log(data)
-                await bot.sendMessage(chatId, 'Спасибо за обратную связь!')
-                await bot.sendMessage(chatId, 'Операционная система: ' + data?.os);
-                await bot.sendMessage(chatId, 'Показывать товары до: ' + data?.maxPrice);
-                await bot.sendMessage(chatId, 'Показывать модели: ' + data?.model);
-    
-                setTimeout(async () => {
-                    await bot.sendMessage(chatId, 'Всю информацию вы получите в этом чате');
-                }, 3000)
-            } catch (e) {
-                console.log(e);
-            }
-        }
-    });
-    
-    app.post('/web-data', async (req, res) => {
-        const {queryId, products = [], totalPrice} = req.body;
-        bot.answerWebAppQuery(queryId, {
-                type: 'article',
-                id: queryId,
-                title: 'Успешная покупка',
-                input_message_content: {
-                    message_text: ` Поздравляю с покупкой, вы приобрели товар на сумму ${totalPrice}, ${products.map(item => item.title).join(', ')}`
-                }
-                
-            })
-        res.status(200).json({});    
-    })
-    
-  }).listen(8000);
+  }).listen(8080);
 
 
 app.listen(PORT, () => console.log('server started on PORT ' + PORT))
